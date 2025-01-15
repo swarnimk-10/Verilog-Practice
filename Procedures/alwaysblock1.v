b0c8df389b0de8590785ea19a7527e65d708a914
Swarnim <135417168+swarnimk-10@users.noreply.github.com>
2025-01-15 20:09:09 +0530

    Update alwaysblock1.v

--- Procedures/alwaysblock1.v
+++ Procedures/alwaysblock1.v
@@ -4,5 +4,9 @@ module top_module(
     output wire out_assign,
     output reg out_alwaysblock
 );
-
+assign out_assign = a&b;
+    always @(*)
+        begin
+            out_alwaysblock = a&b;
+        end
 endmodule
